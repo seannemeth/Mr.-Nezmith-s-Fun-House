57fa05eb38c34fdf0aae13005ee7334979a9c437
seannemeth <sean.nemeth@broadfording.com>
2026-01-21 15:47:17 -0500

    Create GENERIC_FBS_TEAMS constant with team names
    Added a list of generic FBS team names to the project.

---
+++ data/genericFbsTeams.ts
@@ -0,0 +1,143 @@
+export const GENERIC_FBS_TEAMS: string[] = [
+  "Tuscaloosa Iron",
+  "Auburn Plains",
+  "Huntsville Hawks",
+
+  "Phoenix Suns",
+  "Tempe Storm",
+  "Tucson Cats",
+
+  "Fayetteville Boars",
+  "Jonesboro Wolves",
+
+  "Little Rock Trojans",
+
+  "Bay Area Bears",
+  "Los Angeles Bruins",
+  "San Diego Waves",
+  "San Jose Hawks",
+
+  "Boulder Peaks",
+  "Fort Collins Rams",
+
+  "Hartford Shields",
+
+  "Newark Blue",
+  
+  "Tallahassee Chiefs",
+  "Gainesville Swamp",
+  "Miami Storm",
+  "Orlando Knights",
+  "Tampa Bayfires",
+
+  "Athens Hounds",
+  "Atlanta Techs",
+  "Savannah Eagles",
+
+  "Honolulu Tides",
+
+  "Boise Peaks",
+
+  "Champaign Fields",
+  "Evanston Cats",
+  "Chicago Fires",
+
+  "Bloomington Reds",
+  "West Lafayette Engineers",
+
+  "Iowa City Hawks",
+  "Ames Cyclones",
+
+  "Lawrence Plains",
+  "Manhattan Cats",
+
+  "Lexington Blue",
+  "Louisville Flyers",
+
+  "Baton Rouge Stripes",
+  "New Orleans Currents",
+  "Lafayette Fires",
+
+  "Orono Pines",
+
+  "College Park Turtles",
+  "Baltimore Harbors",
+  "Annapolis Mariners",
+
+  "Boston Commons",
+  "Amherst Colonials",
+
+  "Ann Arbor Oaks",
+  "East Lansing Greens",
+
+  "Minneapolis Northmen",
+
+  "Oxford Rebels",
+  "Starkville Maroons",
+
+  "Columbia Plains",
+
+  "Bozeman Range",
+
+  "Lincoln Plainsmen",
+  "Omaha Rivermen",
+
+  "Reno Peaks",
+  "Las Vegas Lights",
+
+  "Albuquerque Suns",
+
+  "Durham Scholars",
+  "Chapel Hill Pines",
+  "Raleigh Capitals",
+
+  "Akron Zips",
+  "Cincinnati Rivercats",
+  "Columbus Union",
+  "Toledo Rockets",
+
+  "Norman Plains",
+  "Stillwater Riders",
+
+  "Eugene Greens",
+  "Corvallis Timber",
+
+  "State College Highlands",
+  "Pittsburgh Ironmen",
+  "Philadelphia Owls",
+
+  "Providence Anchors",
+
+  "Columbia Palmettos",
+
+  "Sioux Falls Plains",
+
+  "Knoxville Ridge",
+  "Nashville Notes",
+  "Memphis Blues",
+
+  "Austin Rangers",
+  "College Station Plains",
+  "Lubbock Dust",
+  "San Antonio Runners",
+  "El Paso Miners",
+  "Dallas Stallions",
+  "Houston Comets",
+
+  "Salt Lake Peaks",
+  "Provo Valleys",
+
+  "Burlington Greens",
+
+  "Charlottesville Cavaliers",
+  "Blacksburg Ridge",
+
+  "Seattle Rain",
+  "Pullman Fields",
+
+  "Morgantown Range",
+
+  "Madison Northwoods",
+
+  "Laramie Plains"
+];
